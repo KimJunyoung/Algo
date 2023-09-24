67ab96faa7650a1ea64bd0519697d6f13ba19dbf
Kimjunyoung <985151@naver.com>
2023-09-24 16:38:58 +0900

    [Gold]BackTracking_algo

---
+++ src/FindAllAlgo/MakePassword.java
@@ -0,0 +1,77 @@
+package FindAllAlgo;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MakePassword {
+
+    // 모음 최소 한개, 자음 최소 두개의 조합 만들기 -> 조합 전체를 만들고 체크를 하자.
+    // 조합 진행시 정렬 해야함 (중복 X, 숫자 선택이랑 동일)
+
+    static int M,N;
+    static int[] selected, inputArr, isUsed;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void rec_func(int k){
+        if(k == M+1){
+            int monCount = 0;
+            int ZaCount = 0;
+
+            for(int i=1; i<=M; i++) {
+                if ((char) selected[i] == 'a' || (char) selected[i] == 'e' || (char) selected[i] == 'i'
+                        || (char) selected[i] == 'o' || (char) selected[i] == 'u') {
+                    monCount++;
+                } else {
+                    ZaCount++;
+                }
+            }
+
+                if(monCount >=1 && ZaCount >=2){
+                    for(int i=1; i<=M; i++){
+                        sb.append((char) selected[i]);
+                    }
+                    sb.append('\n');
+                }
+        }else {
+            for(int cond = 1; cond <= N; cond++){
+                if(isUsed[inputArr[cond]] == 1) continue;
+                if(selected[k-1]  >= inputArr[cond]) continue;
+
+                selected[k] = inputArr[cond];
+                isUsed[inputArr[cond]] = 1;
+
+                rec_func(k+1);
+
+                isUsed[inputArr[cond]] = 0;
+                selected[k] = 0;
+            }
+        }
+    }
+
+    public static void input() throws FileNotFoundException {
+        System.setIn(new FileInputStream("src/input.txt"));
+        Scanner sc = new Scanner(System.in);
+
+        M = sc.nextInt();
+        N = sc.nextInt();
+
+        selected = new int[M+1];
+        inputArr = new int[N+1];
+        isUsed = new int[129];
+
+        for(int i = 1; i<= N; i++) {
+            inputArr[i] = sc.next().charAt(0);
+        }
+
+        Arrays.sort(inputArr);
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        input();
+        rec_func(1);
+        System.out.println(sb);
+    }
+}
